b17cbc236fa40ce06c0448dbb9bafe2b3215d9ce
Aleksandr Piven <alexpvn69@gmail.com>
2024-05-01 21:11:55 +0300

    fix: link to app

--- app/components/WelcomePage/WelcomePageContent/CustomButton.tsx
+++ app/components/WelcomePage/WelcomePageContent/CustomButton.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import '@/app/components/WelcomePage/WelcomePageContent/index.css'
 import { Button } from "@nextui-org/react";
 import { Chakra_Petch } from "next/font/google";
-import Link from "next/link";
 
 const chakraPetch = Chakra_Petch({ weight: '600', subsets: ['latin'] })
 
@@ -11,11 +10,9 @@ const chakraPetch = Chakra_Petch({ weight: '600', subsets: ['latin'] })
 const EnterAppButton = () => {
     return (
         <div className={chakraPetch.className}>
-            <Link href='/app'>
-                <Button isDisabled className="svg-button">
-                    App Soon
-                </Button>
-            </Link>
+            <Button isDisabled className="svg-button">
+                App Soon
+            </Button>
 
         </div>
     )
