5914d86ff53360763cb93b32ecbf1281f64a3c69
0xduda <nkondrashinnn@gmail.com>
2024-05-05 23:35:08 +0500

    fix key

--- app/components/Common/CardContainer.tsx
+++ app/components/Common/CardContainer.tsx
@@ -4,42 +4,27 @@ import ProjectCard from "./ProjectCard";
 
 const CardContainer = () => {
 
-    const numCards = 6; // Минимальное количество карточек для покрытия всего экрана
-    const cards = Array.from({ length: numCards * 2 }); // Удваиваем количество для плавности
+    const numCards = 6;
+    const cards = Array.from({ length: numCards * 2 });
   
-    // return (
-    //     <motion.div
-    //       className="flex gap-3"
-    //       initial={{ translateX: 0 }}
-    //       animate={{ translateX: '-100%' }}
-    //       transition={{
-    //         repeat: Infinity,
-    //         repeatType: "loop",
-    //         duration: 300 // Убедитесь, что 'duration' указан как число
-    //       }}
-    //     >
-    //       {Array.from({ length: 12 }).map((_, index) => (
-    //         <LoadingCard key={index} />
-    //       ))}
-    //     </motion.div>
-    //   );
 
     return (
         <div style={{ width: '100%', overflow: 'hidden' }}>
         <motion.div
           className="flex gap-[50px]"
           initial={{ x: '0%' }}
-          animate={{ x: '-50%' }} // Сдвигаем на 50%, что соответствует полной ширине первого набора карточек
+          animate={{ x: '-50%' }} 
           transition={{
             repeat: Infinity,
             repeatType: "loop",
             duration: 30,
-            ease: "linear" // Обеспечиваем линейную анимацию без замедления
+            ease: "linear"
           }}
-          style={{ width: '200%' }} // Устанавливаем ширину контейнера в 200%
+          style={{ width: '200%' }} 
         >
           {cards.map((_, index) => (
                <motion.div
+               key={index}
                initial={{ opacity: 0, y: 50 }}
                animate={{ opacity: 1, y: 0 }}
                transition={{ duration: 0.5, delay: 0 }}
