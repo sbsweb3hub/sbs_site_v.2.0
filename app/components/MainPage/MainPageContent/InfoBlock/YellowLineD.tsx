2c41a4978334a5baf195c605e474335c7809032c
Vladimir Solo <freeman.wushu@mail.ru>
2024-05-16 10:52:32 +0300

    fix syntax in svg files

--- app/components/MainPage/MainPageContent/InfoBlock/YellowLineD.tsx
+++ app/components/MainPage/MainPageContent/InfoBlock/YellowLineD.tsx
@@ -3,16 +3,16 @@ import '@/app/components/MainPage/MainPageContent/index.css'
 const YellowLineD = () => {
     return (
         <svg viewBox="0 0 1728 76" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line x1="-89" y1="45" x2="648" y2="45" stroke="url(#paint0_linear_323_3315)" stroke-width="2"/>
-            <line y1="-1" x2="737" y2="-1" transform="matrix(-1 0 0 1 1831 46)" stroke="url(#paint1_linear_323_3315)" stroke-width="2"/>
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M616 2.64899C623.934 25.775 652.912 68.3245 718.446 68.3245H870.446H870.946H1022.95C1088.48 68.3245 1117.46 25.775 1125.39 2.64899L1123.5 1.99999C1115.77 24.5406 1087.41 66.3245 1022.95 66.3245H870.946H870.446H718.446C653.98 66.3245 625.625 24.5406 617.892 1.99999L616 2.64899Z" fill="#FCFC03"/>
+            <line x1="-89" y1="45" x2="648" y2="45" stroke="url(#paint0_linear_323_3315)" strokeWidth="2"/>
+            <line y1="-1" x2="737" y2="-1" transform="matrix(-1 0 0 1 1831 46)" stroke="url(#paint1_linear_323_3315)" strokeWidth="2"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M616 2.64899C623.934 25.775 652.912 68.3245 718.446 68.3245H870.446H870.946H1022.95C1088.48 68.3245 1117.46 25.775 1125.39 2.64899L1123.5 1.99999C1115.77 24.5406 1087.41 66.3245 1022.95 66.3245H870.946H870.446H718.446C653.98 66.3245 625.625 24.5406 617.892 1.99999L616 2.64899Z" fill="#FCFC03"/>
             <g filter="url(#filter0_df_323_3315)">
-                <line x1="751" y1="67.5" x2="1023" y2="67.5" stroke="url(#paint2_linear_323_3315)" shape-rendering="crispEdges"/>
+                <line x1="751" y1="67.5" x2="1023" y2="67.5" stroke="url(#paint2_linear_323_3315)" shapeRendering="crispEdges"/>
             </g>
             <line x1="751" y1="67.5" x2="1023" y2="67.5" stroke="url(#paint3_linear_323_3315)"/>
             <g opacity="0.34">
                 <mask id="mask0_323_3315" style={{maskType: 'alpha'}} maskUnits="userSpaceOnUse" x="620" y="-1" width="491" height="67">
-                    <path d="M730.5 64.5C662.142 68.3404 630.408 27.321 621.937 3.83359C621.429 2.42669 622.504 1 624 1L1063.96 0.125326C1104.99 0.0437534 1127.47 22.7437 1092.97 44.9528C1090.88 46.2947 1088.73 47.6447 1086.5 49C1083.89 50.4791 1081.17 51.8546 1078.39 53.1453C1058.88 62.1863 1037.04 64.5 1015.54 64.5H730.5Z" fill="#232323" stroke="black" stroke-width="2"/>
+                    <path d="M730.5 64.5C662.142 68.3404 630.408 27.321 621.937 3.83359C621.429 2.42669 622.504 1 624 1L1063.96 0.125326C1104.99 0.0437534 1127.47 22.7437 1092.97 44.9528C1090.88 46.2947 1088.73 47.6447 1086.5 49C1083.89 50.4791 1081.17 51.8546 1078.39 53.1453C1058.88 62.1863 1037.04 64.5 1015.54 64.5H730.5Z" fill="#232323" stroke="black" strokeWidth="2"/>
                 </mask>
                 <g mask="url(#mask0_323_3315)">
                     <rect x="642" y="32" width="466" height="34" fill="url(#paint4_linear_323_3315)"/>
@@ -22,8 +22,8 @@ const YellowLineD = () => {
                 <ellipse cx="867.5" cy="66.5" rx="120.5" ry="0.5" fill="white"/>
             </g>
             <defs>
-                <filter id="filter0_df_323_3315" x="743" y="59" width="288" height="17" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <filter id="filter0_df_323_3315" x="743" y="59" width="288" height="17" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
                     <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
                     <feOffset/>
                     <feGaussianBlur stdDeviation="4"/>
@@ -33,32 +33,32 @@ const YellowLineD = () => {
                     <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_323_3315" result="shape"/>
                     <feGaussianBlur stdDeviation="2" result="effect2_foregroundBlur_323_3315"/>
                 </filter>
-                <filter id="filter1_f_323_3315" x="741" y="60" width="253" height="13" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <filter id="filter1_f_323_3315" x="741" y="60" width="253" height="13" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
                     <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
                     <feGaussianBlur stdDeviation="3" result="effect1_foregroundBlur_323_3315"/>
                 </filter>
                 <linearGradient id="paint0_linear_323_3315" x1="648" y1="46" x2="-89" y2="46" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#FCFC03"/>
-                    <stop offset="1" stop-color="#FFA600" stop-opacity="0"/>
+                    <stop stopColor="#FCFC03"/>
+                    <stop offset="1" stopColor="#FFA600" stopOpacity="0"/>
                 </linearGradient>
                 <linearGradient id="paint1_linear_323_3315" x1="737" y1="0" x2="0" y2="0" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#FCFC03"/>
-                    <stop offset="1" stop-color="#FFA600" stop-opacity="0"/>
+                    <stop stopColor="#FCFC03"/>
+                    <stop offset="1" stopColor="#FFA600" stopOpacity="0"/>
                 </linearGradient>
                 <linearGradient id="paint2_linear_323_3315" x1="1023" y1="68" x2="751" y2="68" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#FFE0A7" stop-opacity="0"/>
-                    <stop offset="0.498593" stop-color="#FFE0A7"/>
-                    <stop offset="1" stop-color="#FFE0A7" stop-opacity="0"/>
+                    <stop stopColor="#FFE0A7" stopOpacity="0"/>
+                    <stop offset="0.498593" stopColor="#FFE0A7"/>
+                    <stop offset="1" stopColor="#FFE0A7" stopOpacity="0"/>
                 </linearGradient>
                 <linearGradient id="paint3_linear_323_3315" x1="1023" y1="68" x2="751" y2="68" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#FFE0A7" stop-opacity="0"/>
-                    <stop offset="0.498593" stop-color="#FFE0A7"/>
-                    <stop offset="1" stop-color="#FFE0A7" stop-opacity="0"/>
+                    <stop stopColor="#FFE0A7" stopOpacity="0"/>
+                    <stop offset="0.498593" stopColor="#FFE0A7"/>
+                    <stop offset="1" stopColor="#FFE0A7" stopOpacity="0"/>
                 </linearGradient>
                 <linearGradient id="paint4_linear_323_3315" x1="862.291" y1="66.5" x2="862.291" y2="37" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#FCFC03"/>
-                    <stop offset="1" stop-color="#FFD07A" stop-opacity="0"/>
+                    <stop stopColor="#FCFC03"/>
+                    <stop offset="1" stopColor="#FFD07A" stopOpacity="0"/>
                 </linearGradient>
             </defs>
         </svg>
