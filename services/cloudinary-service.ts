2064e733d2875af02ce737628167493a1b0baa7f
Aleksandr Piven <aleksandr.piven@acmecryptocorp.io>
2024-05-23 19:09:02 +0300

    fix env

--- services/cloudinary-service.ts
+++ services/cloudinary-service.ts
@@ -4,7 +4,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import { deleteImageFromProject } from './project-service';
 
 cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
