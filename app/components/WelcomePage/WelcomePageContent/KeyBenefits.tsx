b1bae7107ae329659d968203671e7c74b447ba65
Vladimir Solo <freeman.wushu@mail.ru>
2024-06-05 07:23:27 +0300

    пофиксил ошибку в KeyBenefits

--- app/components/WelcomePage/WelcomePageContent/KeyBenefits.tsx
+++ app/components/WelcomePage/WelcomePageContent/KeyBenefits.tsx
@@ -6,8 +6,8 @@ const KeyBenefits = () => {
         <CustomAccordion label="Key Benefits">
             <p>
                 The project roadmap is segmented into distinct stages,<br/> each with a well-defined plan and schedule.<br/><br/>
-                Project financing is structured in installments. Each stage is accompanied by a comprehensive report detailing the team's actual results and achievements.<br/><br/>
-                Smart contracts ensure that the team can access funds only upon successful completion of the previous stage. After reviewing the report, investors can vote to approve or deny subsequent transactions.
+                Project financing is structured in installments. Each stage is accompanied by a comprehensive report detailing the team&apos;s actual results and achievements.<br/><br/>
+                Smart &apos contracts ensure that the team can access funds only upon successful completion of the previous stage. After reviewing the report, investors can vote to approve or deny subsequent transactions.
             </p>
         </CustomAccordion>
     )
