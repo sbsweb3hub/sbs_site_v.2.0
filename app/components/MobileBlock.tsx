265f6cedeb8f8b17646689b9e7346e5adf28698e
0xduda <nkondrashinnn@gmail.com>
2024-09-15 12:05:23 +0500

    mobile blocked

---
+++ app/components/MobileBlock.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MobileBlock = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen text-center px-[20px]'>
+      <h1 className='mb-[10px]'>The mobile version is under development.</h1>
+      <p>Please use the desktop version of the website to access the application.</p>
+      <p className='mt-[10px]'>We apologize for the inconvenience.</p>
+    </div>
+  );
+};
+
+export default MobileBlock;
